f50c3a4137629a4bac74972520e7b11b227ad48c
Meera srilekha.B <117934529+Meerasrilekha@users.noreply.github.com>
2024-10-23 00:41:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.querySelector('#menu-icon');
+    const navbar = document.querySelector('.navbar');
+
+    menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+    });
+    window.addEventListener('scroll', () => {
+        if (window.innerWidth > 768) {
+            navbar.classList.remove('active');
+        }
+    });
+    
+
+    
+});
